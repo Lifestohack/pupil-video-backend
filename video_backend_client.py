48524adde288d550ab389aec66521b4265c2ea31
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-10 14:57:32 +0000

    connect with zmq_tools but not in client

--- video_backend_client.py
+++ video_backend_client.py
@@ -2,7 +2,7 @@ from zmq_tools import *
 import traceback
 addr = '192.168.0.213'  # remote ip or localhost
 sub_port = "50020"  # same as in the pupil remote gui
-sub.connect("tcp://{}:{}".format(addr, sub_port))
+ipc_sub_url = "tcp://{}:{}".format(addr, sub_port)
 zmq_ctx = zmq.Context()
 sub = Msg_Receiver(zmq_ctx, ipc_sub_url, topics=("hmd_streaming.world",), block_until_connected=True)
 
