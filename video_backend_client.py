d644c625e1b6c4ae0a8d688aa14ae519e418a99d
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-10 15:35:17 +0100

    variable renamed

--- video_backend_client.py
+++ video_backend_client.py
@@ -4,11 +4,11 @@ addr = '192.168.0.213'  # remote ip or localhost
 req_port = "50020"  # same as in the pupil remote gui
 ipc_sub_url = "tcp://{}:{}".format(addr, req_port)
 zmq_ctx = zmq.Context()
-notify_sub = Msg_Receiver(zmq_ctx, ipc_sub_url, topics=("hmd_streaming.world",), block_until_connected=True)
+sub = Msg_Receiver(zmq_ctx, ipc_sub_url, topics=("hmd_streaming.world",), block_until_connected=True)
 
 try:
     while True:
-        topic, msg = notify_sub.recv_topic()
+        topic, msg = sub.recv_topic()
         print(topic)
 except Exception:
     pass
