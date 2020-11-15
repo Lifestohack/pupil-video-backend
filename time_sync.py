ac9c9ce68e8ab9f0162296d79ad2acdbf9028110
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-15 03:36:01 +0100

    time can be epoch or monotonic. pass it while calling Clock_Follower

--- time_sync.py
+++ time_sync.py
@@ -25,7 +25,8 @@ class Clock_Follower():
     
     """
 
-    def __init__(self, pupil_remote):
+    def __init__(self, pupil_remote, time):
+        self.time = time
         self.pupil_remote = pupil_remote
         mean_offset, offset_jitter = self.get_offsets()
         self.offset = abs(mean_offset) + abs(offset_jitter)
@@ -37,25 +38,24 @@ class Clock_Follower():
     def get_offsets(self):
         times = []
         for _ in range(60):
-            t0 = monotonic()
+            t0 = self.time()
             t1 = self.get_pupil_time()
-            t2 = monotonic() 
+            t2 = self.time()
             times.append((t0, t1, t2))
-        times.sort(key=lambda t: t[2]-t[0])
-        times = times[:int(len(times)*0.69)]
+        times.sort(key=lambda t: t[2] - t[0])
+        times = times[:int(len(times) * 0.69)]
 
         # assuming latency on both direction to be same
         offsets = [t0 - ((float(t1) + (t2 - t0) / 2)) for t0, t1, t2 in times]
         mean_offset = sum(offsets) / len(offsets)
         
-        #Jitter is the deviation from the clock's ideal behavior. 
-        # The clock edges aren't where you expected them to.
+        # Jitter is the deviation from the clock's ideal behavior. 
+        # It means the clock edges weren't where you expected them to.
         offset_jitter = sum([abs(mean_offset - o) for o in offsets]) / len(offsets)
         return mean_offset, offset_jitter
 
     def get_synced_pupil_time(self, localtime):
         """
         Returns time after sync.
-        localtime is monotonic as offset was calculated using monotonic.
         """
         return localtime + self.offset
