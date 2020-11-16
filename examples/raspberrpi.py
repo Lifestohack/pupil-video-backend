08de1681f30ff6fb4c4c8773360459c8d9f643a0
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-16 15:40:21 +0100

    examples updated

--- examples/raspberrpi.py
+++ examples/raspberrpi.py
@@ -1,5 +1,5 @@
 from video_backend import VideoBackEnd
-from picamera import PiCamera
+import picamera
 from picamera.array import PiRGBArray
 from picamera.array import PiYUVArray
 import numpy as np
@@ -8,6 +8,8 @@ import sys
 from payload import Payload
 import traceback
 from time import sleep, time
+import log
+import logging
 
 # Helper class implementing an IO deamon thread
 class StartThreadToStream:
@@ -28,7 +30,7 @@ class StartThreadToStream:
                 #f = self.payload[0][:,:,0]      # If only brightness information is needed from yuv format and not rgb 
                 self.pub_socket.send(self.payload)
             else:
-                time.sleep(0.0001)
+                sleep(0.0001)
 
     def dataready(self, payload):
         self.payload = payload
@@ -36,7 +38,6 @@ class StartThreadToStream:
 
     def close(self):
         self._stop = True
-
 
 ip = "127.0.0.1"    # ip address of remote pupil or localhost
 port = "50020"      # same as in the pupil remote gui
@@ -51,54 +52,53 @@ def streamVideo():
     pub_socket = backend.get_msg_streamer()
     # Make sure to set up raspberry pi camera
     # More information here: https://www.raspberrypi.org/documentation/configuration/camera.md
-    camera = PiCamera()
-    sleep(2.0)  # Warmup time; needed by PiCamera on some RPi's
-    # set camera parameters
-    camera.resolution = resolution
-    camera.framerate = framerate
-    # Pupils capture software rotates HMD Streaming Source 180 degree.
-    # Sending rotated video so it shows correctly
-    camera.rotation = 180    
-    rawCapture = PiRGBArray(camera, size=resolution)
-    #rawCapture = PiYUVArray(camera, size=resolution)
-    stream = camera.capture_continuous(rawCapture, format="rgb", use_video_port=True)
-    frame_counter_per_sec = 0
-    frame_index = 1
-    #streamimage = StartThreadToStream(pub_socket)
-    payload = Payload(device, resolution[0], resolution[1])
-    fps = 0
-    start_time = time()
-    try:
-        for f in stream:
-            if backend.is_publishable():
-                # grab the frame from the stream and clear the stream in
-                # preparation for the next frame
-                frame = f.array
-                payload.setPayloadParam(time(), frame, frame_index)
-                #streamimage.dataready(payload.get())    #   give it to StartThreadToStream to publish
-                pub_socket.send(payload.get())           #   publish here
-                seconds = time() - start_time
-                if seconds > 1:
-                    fps = frame_counter_per_sec
-                    frame_counter_per_sec = 0
-                    start_time = time()
-                outstr = "Frames: {}, FPS: {}".format(frame_index, fps) 
-                sys.stdout.write('\r'+ outstr)
-                frame_counter_per_sec = frame_counter_per_sec + 1
-                frame_index = frame_index + 1
-                rawCapture.truncate(0)
-            else:
-                break
-    except (KeyboardInterrupt, SystemExit):
-        print('Exit due to keyboard interrupt')
-    except Exception as ex:
-        print('Python error with no Exception handler:')
-        print('Traceback error:', ex)
-        traceback.print_exc()
-    finally:
-        #streamimage.close()
-        camera.close()
-        sleep(5)
+    with picamera.PiCamera() as camera:
+        # sleep(2.0)  # Warmup time; needed by PiCamera on some RPi's
+        # set camera parameters
+        camera.resolution = resolution
+        camera.framerate = framerate
+        rawCapture = PiRGBArray(camera, size=resolution)
+        #rawCapture = PiYUVArray(camera, size=resolution)
+        stream = camera.capture_continuous(rawCapture, format="rgb", use_video_port=True)
+        frame_counter_per_sec = 0
+        frame_index = 1
+        #streamimage = StartThreadToStream(pub_socket)
+        payload = Payload(device, resolution[0], resolution[1])
+        fps = 0
+        start_time = time()
+        try:
+            for f in stream:
+                if backend.is_publishable():
+                    # grab the frame from the stream and clear the stream in
+                    # preparation for the next frame
+                    frame = f.array
+                    payload.setPayloadParam(time(), frame, frame_index)
+                    #streamimage.dataready(payload.get())    #   give it to StartThreadToStream to publish
+                    pub_socket.send(payload.get())           #   publish here
+                    seconds = time() - start_time
+                    if seconds > 1:
+                        fps = frame_counter_per_sec
+                        frame_counter_per_sec = 0
+                        start_time = time()
+                    #outstr = "Frames: {}, FPS: {}".format(frame_index, fps) 
+                    #sys.stdout.write('\r'+ outstr)
+                    frame_counter_per_sec = frame_counter_per_sec + 1
+                    frame_index = frame_index + 1
+                    rawCapture.truncate(0)
+                else:
+                    break
+        except (KeyboardInterrupt, SystemExit):
+            logging.info('Exit due to keyboard interrupt')
+        except Exception:
+            exp = traceback.format_exc()
+            logging.error(exp)
+        finally:
+            #streamimage.close()
+            del stream
+            del rawCapture
+            del payload
+            logging.info("Stopping the stream for device: {}.".format(device))
+            logging.info("Total Published frames: {}, FPS:{}.".format(frame_index, fps))
 
 if __name__ == "__main__":
     backend.start(device, callback=streamVideo)
