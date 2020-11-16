1ee85c80e815edf632fcf1b99cf702377544bc53
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-16 20:39:37 +0100

    added dummy intrinsics. Warning do not put 0 as values. Pupil capture can't work with that

--- payload.py
+++ payload.py
@@ -2,16 +2,21 @@ class Payload:
 
     def __init__(self, topic, width, height, intrinsics=None):
         if intrinsics is None:
-            intrinsics = [
-                        [0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0],
-                    ]
+            # intrinsics = [
+            #             [0.0, 0.0, 0.0],
+            #             [0.0, 0.0, 0.0],
+            #             [0.0, 0.0, 0.0],
+            #         ]
+             intrinsics = [
+                        [1000, 0.0, 192 / 2.0],
+                        [0.0, 1000, 192 / 2.0],
+                        [0.0, 0.0, 1.0],
+                        ]
         if topic is None:
             raise Exception("topic has to be either: world, eye0 or eye1")
 
         self.payload = {}
-        self.payload["format"] = "rgb"
+        self.payload["format"] = "bgr"
         self.payload["projection_matrix"] = intrinsics
         self.payload["topic"] = "hmd_streaming." + topic
         self.payload["width"] = width
