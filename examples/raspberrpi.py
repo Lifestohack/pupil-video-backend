bd63eee800c5e8ffd7a61a4e8e49c972f01a1235
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-22 22:13:16 +0000

    raspberrypi.py example updated

--- examples/raspberrpi.py
+++ examples/raspberrpi.py
@@ -7,88 +7,112 @@ import threading
 import sys
 from payload import Payload
 import traceback
-from time import sleep, time
+from time import sleep, time, monotonic
 import log
 import logging
 import numpy
 import cv2
+from fractions import Fraction
 
-# Helper class implementing an IO deamon thread
-class StartThreadToStream:
+''' 
+    This example is just meant to send the Y part of YUV for the eye0 process. 
+    Send one channel gray frame to pupil capture software.
+'''
 
-    def __init__(self, pub_socket):
-        self.newpayload = False
-        self.payload = None
-        self.pub_socket = pub_socket
-        self._stop = False
-        self._thread = threading.Thread(target=self._run, args=())
-        self._thread.daemon = True
-        self._thread.start()
-    
-    def _run(self):
-        while not self._stop:
-            if self.newpayload == True:
-                self.newpayload = False
-                #f = self.payload[0][:,:,0]      # If only brightness information is needed from yuv format and not rgb 
-                self.pub_socket.send(self.payload)
-            else:
-                sleep(0.0001)
-
-    def dataready(self, payload):
-        self.payload = payload
-        self.newpayload = True
-
-    def close(self):
-        self._stop = True
-
-ip = "192.168.0.188"    # ip address of remote pupil or localhost
+ip = "127.0.0.1"    # ip address of remote pupil or localhost
 port = "50020"      # same as in the pupil remote gui
 device = "eye0"
 
 # initialize the stream
 backend = VideoBackEnd(ip, port)
 
+# Helper class implementing an IO deamon thread
+class StartThreadToStream:
+    '''
+        Using new thread to send the the frame
+    '''
+    def __init__(self, pub_socket, device, resolution, backend):
+        self.backend = backend
+        self.newpayload = False
+        self.frame = None
+        self.frame_index = 1
+        self.monotonic = 0
+        self.pub_socket = pub_socket
+        self._stop = False
+        self._thread = threading.Thread(target=self._run, args=())
+        self._thread.daemon = True
+        self._thread.start()
+        self.payload = Payload(device, resolution[0], resolution[1], "gray")
+    
+    def _run(self):
+        while not self._stop:
+            if self.newpayload == True:
+                self.newpayload = False
+                frame = numpy.ascontiguousarray(self.frame)
+                self.payload.setPayloadParam(self.backend.get_synced_pupil_time(self.monotonic), frame, self.frame_index)
+                self.pub_socket.send(self.payload.get())
+            else:
+                sleep(0.001)
+
+    def dataready(self, frame, frame_index, time_monotonic):
+        self.frame = frame
+        self.frame_index = frame_index
+        self.monotonic = time_monotonic
+        self.newpayload = True
+
+    def close(self):
+        self._thread.join()
+        self._stop = True
+
 def streamVideo():
     resolution =  (192, 192)
-    framerate = 90
+    fps = 90
     pub_socket = backend.get_msg_streamer()
     # Make sure to set up raspberry pi camera
     # More information here: https://www.raspberrypi.org/documentation/configuration/camera.md
     with picamera.PiCamera() as camera:
-        # sleep(2.0)  # Warmup time; needed by PiCamera on some RPi's
         # set camera parameters
         camera.resolution = resolution
-        camera.framerate = framerate
+        camera.framerate = Fraction(fps, 1)
+        #camera.sensor_mode = 7
+        #camera.exposure_mode = 'off'
+        #camera.shutter_speed = 6000000
+        #camera.iso = 1600
         #rawCapture = PiRGBArray(camera, size=resolution)
         rawCapture = PiYUVArray(camera, size=resolution)
         stream = camera.capture_continuous(rawCapture, format="yuv", use_video_port=True)
         frame_counter_per_sec = 0
         frame_index = 1
-        #streamimage = StartThreadToStream(pub_socket)
-        payload = Payload(device, resolution[0], resolution[1], "gray")
+        streamimage = StartThreadToStream(pub_socket, device, resolution, backend)
+        #payload = Payload(device, resolution[0], resolution[1], "gray")    # use this if sending without new thread StartThreadToStream
         fps = 0
         start_time = time()
+        image_read_time = time()
         try:
             for f in stream:
                 if backend.is_publishable():
                     # grab the frame from the stream and clear the stream in
                     # preparation for the next frame
                     frame = f.array
-                    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                    frame = numpy.ascontiguousarray(frame[:,:,0])
-                    payload.setPayloadParam(time(), frame, frame_index)
-                    #streamimage.dataready(payload.get())    #   give it to StartThreadToStream to publish
+                    capture_time = monotonic()
+                    latency = time() - image_read_time
+                    streamimage.dataready(frame[:,:,0], frame_index, capture_time)    #   give it to StartThreadToStream to publish
+                    ''' 
+                    # use this also if sending without new thread StartThreadToStream but deactivate above line
+                    payload.setPayloadParam(capture_time, numpy.ascontiguousarray(frame[:,:,0]), frame_index)
                     pub_socket.send(payload.get())           #   publish here
+                    '''
                     seconds = time() - start_time
                     if seconds > 1:
                         fps = frame_counter_per_sec
                         frame_counter_per_sec = 0
                         start_time = time()
-                    outstr = "Frames: {}, FPS: {}".format(frame_index, fps) 
+                    outstr = "Frames: {}, FPS: {}, Latency: {}".format(frame_index, fps, latency) 
                     sys.stdout.write('\r'+ outstr)
                     frame_counter_per_sec = frame_counter_per_sec + 1
                     frame_index = frame_index + 1
                     rawCapture.truncate(0)
+                    image_read_time = time()
                 else:
                     break
         except (KeyboardInterrupt, SystemExit):
@@ -97,10 +121,9 @@ def streamVideo():
             exp = traceback.format_exc()
             logging.error(exp)
         finally:
-            #streamimage.close()
+            streamimage.close()
             del stream
             del rawCapture
-            del payload
             logging.info("Stopping the stream for device: {}.".format(device))
             logging.info("Total Published frames: {}, FPS:{}.".format(frame_index, fps))
 
