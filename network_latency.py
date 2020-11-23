836e3bb9c223e242838bfadc4e8b59109adb9d91
Diwas Bhattarai <diwas@bhattarai.de>
2020-11-23 19:50:53 +0000

    added everything in function and in __main__

--- network_latency.py
+++ network_latency.py
@@ -2,12 +2,11 @@ from time import time, sleep
 from pupil import PupilManager
 import log, logging
 
-host = "10.3.141.136"
-port = "50020"
 
-pupil = PupilManager(host, port)
-
-def get_offsets():
+def get_network_latency():
+    host = "10.3.141.136"
+    port = "50020"
+    pupil = PupilManager(host, port)
     times = []
     logging.info("Calculating the network lateny...")
     for _ in range(100):
@@ -18,12 +17,12 @@ def get_offsets():
         times.append((t0, t1))
     # Assuming latency on both direction to be same.
     latency_one_direction = [(t1 - t0) / 2 for t0, t1 in times]
+    pupil.close()
     return min(latency_one_direction), sum(latency_one_direction) / len(latency_one_direction), max(latency_one_direction)
     
-
-min_lat, avg_lat, max_lat = get_offsets()
-logging.info("One direction Network latency calculated between Video backend and Pupil Software.")
-logging.info("Minimum:{} second".format(min_lat))
-logging.info("Average:{} second".format(avg_lat))
-logging.info("Maximum:{} second".format(max_lat))
-pupil.close()
+if __name__ == "__main__":
+    min_lat, avg_lat, max_lat = get_offsets()
+    logging.info("One direction Network latency calculated between Video backend and Pupil Software.")
+    logging.info("Minimum:{} second".format(min_lat))
+    logging.info("Average:{} second".format(avg_lat))
+    logging.info("Maximum:{} second".format(max_lat))
